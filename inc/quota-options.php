ea5e365c1bc9fcf6562f9eb9ee34b757ab90c4d2
Sean Davis <sdavis2702@gmail.com>
2014-05-25 12:24:36 -0500

    change license key page title

--- inc/quota-options.php
+++ inc/quota-options.php
@@ -41,7 +41,7 @@ function quota_options_page() {
 	$status 	= get_option( 'quota_license_key_status' );
 	?>
 	<div class="wrap">
-		<h2><?php _e( QUOTA_NAME .' License Options' ); ?></h2>
+		<h2><?php _e( QUOTA_NAME . ' License Key Management' ); ?></h2>
 		<form method="post" action="options.php">
 		
 			<?php settings_fields( 'quota_license' ); ?>
