2d3093c493905bbda4b552c9f52fc0d7ea909481
Sean Davis <sdavis2702@gmail.com>
2014-07-24 13:17:06 -0500

    update version number

--- inc/quota-functions.php
+++ inc/quota-functions.php
@@ -9,7 +9,7 @@
  */
 define( 'QUOTA_NAME', 'Quota' );
 define( 'QUOTA_AUTHOR', 'Sean Davis' );
-define( 'QUOTA_VERSION', '1.2' );
+define( 'QUOTA_VERSION', '1.2.1' );
 define( 'QUOTA_HOME', 'https://easydigitaldownloads.com' );
 define( 'QUOTA_DIR', get_template_directory() );
 define( 'QUOTA_DIR_URI', get_template_directory_uri() );
